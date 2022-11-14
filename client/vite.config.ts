1708b286cc98e3a4f18f8bba0e55a04987c30b2d
Tobani <tobanigeorge@gmail.com>
2022-11-14 21:47:14 +0100

    added magic.css imports + bug fixes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./src/main.tsx"
-    }
-  }
+  // build: {
+  //   manifest: true,
+  //   rollupOptions: {
+  //     input: "./src/main.tsx"
+  //   }
+  // }
 })
